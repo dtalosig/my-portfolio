011e19eaef58d93bde14a3a49d769b656c46b243
unknown <danielmtalosig@gmail.com>
2017-09-17 18:08:57 +0800

    banner is done

--- js/javascript.js
+++ js/javascript.js
@@ -6,3 +6,22 @@
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
   });
+
+  $(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+  $('html, body').stop().animate({
+     'scrollTop': $target.offset().top
+}, 900, 'swing');
