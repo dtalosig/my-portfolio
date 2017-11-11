7a3a6bf1493aa91ba391f77a902248aab7e40a06
unknown <danielmtalosig@gmail.com>
2017-11-11 19:00:27 +0800

    done in navbar but not the color

--- js/my-javascript.js
+++ js/my-javascript.js
@@ -2,11 +2,15 @@ $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 100) {
-	    $(".navbar-modifier").css({"opacity" : "0.8"});
+	    $(".navbar-header").css({"padding" : "0", "transition" : ".5s", "opacity" : "0.5"});
+	    $(".navbar-modifier").css({"opacity" : "0.9"});
+	    $(".navbar-collapse").css({"padding-left" : "20", "padding-right" : "20", "transition" : ".5s"});
 	  }
 
 	  else{
+		  $(".navbar-header").css({"padding" : "10", "transition" : ".5s", "opacity" : "1"});  	
 		  $(".navbar-modifier").css({"opacity" : "1"});  	
+		  $(".navbar-collapse").css({"padding" : "10", "padding-left" : "20", "padding-right" : "20", "transition" : ".5s"});  	
 	  }
   })
 
