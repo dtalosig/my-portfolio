c2e2b10effc637fcceae5c381c3fde13f01bbc9e
unknown <danielmtalosig@gmail.com>
2017-11-11 18:36:07 +0800

    almost done in navbar

--- js/my-javascript.js
+++ js/my-javascript.js
@@ -1,1 +1,25 @@
-my-javascript.js
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 100) {
+	    $(".navbar-modifier").css({"opacity" : "0.8"});
+	  }
+
+	  else{
+		  $(".navbar-modifier").css({"opacity" : "1"});  	
+	  }
+  })
+
+   $('a[href^="#"]').on('click', function (e) {
+	     e.preventDefault();
+
+	     var target = this.hash,
+	         $target = $(target);
+
+	     $('html, body').stop().animate({
+	         'scrollTop': $target.offset().top - 80
+	     }, 900, 'swing', function () {
+	         window.location.hash = target;
+	     });
+ 	});
+})
